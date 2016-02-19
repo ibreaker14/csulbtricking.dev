fb2fe723e5e5d35f8f580bc8888e51321604e1fc
ibreaker14 <minijordon@gmail.com>
2016-02-19 10:44:25 -0800

    child theme & plugins installed
    child theme wizard, custom post type ui, toolset types, blackbox debug
    bar, posts to articles

---
+++ wp-content/plugins/posts-to-articles.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @package Posts_To_Articles
+ * @version 1.0
+ */
+/*
+Plugin Name: Posts to Articles
+Plugin URI: http://wordpress.org/
+Description: change standard WP posts to articles
+Author: ibreaker
+Version: 1.0
+Author URI: http://ib/
+*/
+
+function change_post_menu_label() {
+    global $menu;
+    global $submenu;
+    $menu[5][0] = 'Articles';
+    $submenu['edit.php'][5][0] = 'Articles';
+    $submenu['edit.php'][10][0] = 'Add Articles';
+    echo '';
+}
+function change_post_object_label() {
+        global $wp_post_types;
+        $labels = &$wp_post_types['post']->labels;
+        $labels->name = 'Articles';
+        $labels->singular_name = 'Article';
+        $labels->add_new = 'Add Article';
+        $labels->add_new_item = 'Add Article';
+        $labels->edit_item = 'Edit Article';
+        $labels->new_item = 'Article';
+        $labels->view_item = 'View Article';
+        $labels->search_items = 'Search Articles';
+        $labels->not_found = 'No Articles found';
+        $labels->not_found_in_trash = 'No Articles found in Trash';
+}
+add_action( 'init', 'change_post_object_label' );
+add_action( 'admin_menu', 'change_post_menu_label' );
