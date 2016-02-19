696f57eb02b01e63f23b5b7f1b79b3baa40668c2
ibreaker14 <minijordon@gmail.com>
2016-02-19 12:44:06 -0800

    created template files

--- wp-content/themes/awesome-one-child/single-tricker.php
+++ wp-content/themes/awesome-one-child/single-tricker.php
@@ -12,10 +12,12 @@ get_header(); ?>
         <div class="site-main" id="sitemain">
 			<?php while ( have_posts() ) : the_post(); ?>
                 <?php get_template_part( 'content', 'single' ); ?>
+                
                 <?php //types echo custom fields
                 echo types_render_field("profile-photo", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));
                 echo "<br>";
-                echo types_render_field("email-d1d6a7ea", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));
+                echo types_render_field("email-d1d6a7ea", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));echo "<br>";
+                echo types_render_field("phone-number", array("argument1"=>"value1","argument2"=>"value2","argument2"=>"value2"));
                 ?>
                 <?php awesomeone_content_nav( 'nav-below' ); ?>
 
@@ -25,9 +27,6 @@ get_header(); ?>
                 // If comments are open or we have at least one comment, load up the comment template
                 if ( comments_open() || '0' != get_comments_number() )
                     comments_template();
-
-
-
 
                 ?>
             <?php endwhile; // end of the loop. ?>
